92daac6df42c3198b47e7196d636791287653caa
wzf-cn <wzfhrb.cn@gmail.com>
2017-05-10 20:45:18 +0800

    0510

--- ch23/ch23.py
+++ ch23/ch23.py
@@ -53,39 +53,39 @@ card3 = Card(1, 11)
 print(card1 < card2)
 print(card1 == card3)
 
-#class Deck:
-#    def __init__(self):
-#        self.cards = []
-#        for suit in range(4):
-#            for rank in range(1, 14):
-#                self.cards.append(Card(suit, rank))
-#
-#    def __str__(self):
-#        s = ""
-#        for i in range(len(self.cards)):
-#            s = s + " " * i + str(self.cards[i]) + "\n"
-#        return s
-#    
-#    def shuffle(self):
-#        import random
-#        rng = random.Random()        # Create a random generator
-#        rng.shuffle(self.cards)
-#        
-#    def remove(self, card):
-#        if card in self.cards:
-#            self.cards.remove(card)
-#            return True
-#        else:
-#            return False
-#    def pop(self):
-#        return self.cards.pop()
-#    
-#    def is_empty(self):
-#        return self.cards == []
-#        
-#red_deck = Deck()
-#blue_deck = Deck()
-#print(red_deck)
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1, 14):
+                self.cards.append(Card(suit, rank))
+
+    def __str__(self):
+        s = ""
+        for i in range(len(self.cards)):
+            s = s + " " * i + str(self.cards[i]) + "\n"
+        return s
+    
+    def shuffle(self):
+        import random
+        rng = random.Random()        # Create a random generator
+        rng.shuffle(self.cards)
+        
+    def remove(self, card):
+        if card in self.cards:
+            self.cards.remove(card)
+            return True
+        else:
+            return False
+    def pop(self):
+        return self.cards.pop()
+    
+    def is_empty(self):
+        return self.cards == []
+        
+red_deck = Deck()
+blue_deck = Deck()
+print(red_deck)
 
 
 
